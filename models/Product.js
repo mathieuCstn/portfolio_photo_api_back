cf196188f296c226890db0fb3dcf843229342ad0
mathieu_cstn <mathieuconstantin@gmail.com>
2023-06-09 09:35:04 +0200

    New CRUD operations added

--- models/Product.js
+++ models/Product.js
@@ -1,33 +1,89 @@
 const dbconnect = require('../config/dbconnect')
+const cloudinary = require('cloudinary').v2
 
 module.exports = class Product {
     /**
-     * Add a new product in the database.
-     * @param {string} cld_public_id 
-     * @param {number} price 
-     * @param {string} title 
-     * @param {string} description 
-     * @param {number} quantity 
-     * @param {boolean} available 
-     * @returns {Promise<ResultSetHeader>}
+     * Returns a list of all products in the database.
+     * @returns {Promise<[{}]>}
      */
-    static addProduct(cld_public_id, options) {
-        if(!cld_public_id) throw new Error("Product.addProduct() : No 'cld_public_id' parameter found")
-        const optionsDefault = {price: null, title: null, description: null, quantity: null, available: true}
-        const params = {...optionsDefault, ...options}
-        return dbconnect.then(db => db.query(
-            'INSERT INTO products(cld_public_id, price, title, description, quantity, avalible) VALUES(?, ?, ?, ?, ?, ?)', 
-            [cld_public_id, params.price, params.title, params.description, params.quantity, params.available]
-        ))
-            .then(data => data[0])
-            .catch(error => Promise.reject(error))
-    }
-
     static getProducts() {
         return dbconnect.then(db => db.query(
             'SELECT * FROM products'
         ))
             .then(datas => datas[0])
+            .catch(error => Promise.reject(error))
+    }
+
+    /**
+     * Return one product object with its id.
+     * @param {{id: number}} product 
+     * @returns {Promise<>}
+     */
+    static getOneProduct(product) {
+        if(!product.id) throw new Error("Product.getOneProduct() : No 'id' property found in product parameter.")
+        return dbconnect.then(db => db.query(
+            'SELECT * FROM products WHERE id = ? LIMIT 1',
+            [product.id]
+        ))
+            .then(data => data[0][0])
+            .catch(error => Promise.reject(error))
+    }
+
+    /**
+     * Add a new product in the database.
+     * @param {string} cld_public_id 
+     * @param {{price: number, title: string, description: string, quantity: number, avalible: boolean}} options
+     * @returns {Promise<ResultSetHeader>}
+     */
+    static addProduct(cld_public_id, options) {
+        if(!cld_public_id) throw new Error("Product.addProduct() : No 'cld_public_id' parameter found.")
+        const optionsDefault = {price: null, title: null, description: null, quantity: null, avalible: true}
+        const params = {...optionsDefault, ...options}
+        return dbconnect.then(db => db.query(
+            'INSERT INTO products(cld_public_id, price, title, description, quantity, avalible) VALUES(?, ?, ?, ?, ?, ?)', 
+            [cld_public_id, params.price, params.title, params.description, params.quantity, params.avalible]
+        ))
+            .then(data => data[0])
+            .catch(error => Promise.reject(error))
+    }
+
+    /**
+     * Updates a product object in the database with its id.
+     * @param {number} productId 
+     * @param {{cld_public_id: string, title: string, description: string, price: number, quantity: number, avalible: boolean}} options 
+     * @returns {Promise<ResultSetHeader>}
+     */
+    static async updateProduct(productId, options) {
+        if(!productId) throw new Error("Product.updateProduct() : No 'ProductId' parameter found.")
+        const optionsDefault = await this.getOneProduct({id: productId})
+        const params = {...optionsDefault, ...options}
+        return dbconnect.then(db => db.query(
+            'UPDATE products SET cld_public_id = ?, price = ?, title = ?, description = ?, quantity = ?, avalible = ? WHERE id = ?',
+            [params.cld_public_id, params.price, params.title, params.description, params.quantity, params.avalible, productId]
+        ))
+            .then(data => data[0])
+            .catch(error => Promise.reject(error))
+    }
+
+    /**
+     * 
+     * @param {number} productId 
+     * @returns {Promise<ResultSetHeader>}
+     */
+    static async deleteProduct(productId) {
+        if(!Product) throw new Error("Product.deleteProduct() : No 'ProductId' parameter found.")
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET
+        })
+        const { cld_public_id } = await this.getOneProduct({id: productId})
+        cloudinary.uploader.destroy(cld_public_id)
+        return dbconnect.then(db => db.query(
+            'DELETE FROM products WHERE id = ?',
+            [productId]
+        ))
+            .then(results => results[0])
             .catch(error => Promise.reject(error))
     }
 }
--- routes/product.js
+++ routes/product.js
@@ -25,12 +25,35 @@ router.post('/add', verifyRoles(['admin']), upload.single('image'), async (req, 
         const description = req.body?.description ? req.body.description : null
         const price = req.body?.price ? req.body.price : null
         const quantity = req.body?.quantity ? req.body.quantity : null
-        const available = req.body?.available || true
-        const options = {price, title, description, quantity, available}
+        const avalible = req.body?.avalible === 'on' ? 1 : 0
+        const options = {price, title, description, quantity, avalible}
         const results = await Product.addProduct(public_id, options)
         if(results) return res.status(201).json({message: 'New image saved in database'})
     } catch (error) {
-        res.status(500).json({ error })
+        return res.status(500).json({ name: error.name, message: error.message, ...error})
+    }
+})
+
+router.put('/:id', verifyRoles(['admin']), async (req, res) => {
+    try {
+        if(req.body?.avalible === 'on') {
+            req.body.avalible = 1
+        } else {
+            req.body.avalible = 0
+        }
+        const ResultSetHeader = await Product.updateProduct(req.params.id, req.body)
+        res.status(200).json({message: 'Update applied', ResultSetHeader})
+    } catch (error) {
+        return res.status(500).json({ name: error.name, message: error.message, ...error})
+    }
+})
+
+router.delete('/:id', verifyRoles(['admin']), async (req, res) => {
+    try {
+        const ResultSetHeader = await Product.deleteProduct(req.params.id)
+        res.status(200).json({message: 'The product has been successfully removed.', ResultSetHeader})
+    } catch (error) {
+        return res.status(500).json({ name: error.name, message: error.message, ...error})
     }
 })
 
